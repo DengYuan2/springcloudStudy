5ada89f05550fe86a65294bd98e31b647ad11a7c
dyy <2216502193@qq.com>
2020-08-09 17:25:11 +0800

    zookeeper

---
+++ cloud-consumerzk-order80/src/main/java/com/dyy/springcloud/controller/OrderZKController.java
@@ -0,0 +1,26 @@
+package com.dyy.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderZKController {
+    //即zookeeper上注册过的提供者的地址
+    public static final String INVOKE_URL="http://cloud-provider-payment";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/zk")
+    public String paymentInfo(){
+        String result = restTemplate.getForObject(INVOKE_URL + "/payment/zk", String.class);
+        log.info(result);
+        return result;
+    }
+
+}
